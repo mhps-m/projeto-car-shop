ae9af74bf63fb2c3a82c848d67a51fecbc3c0c9f
Miguel Soares <miguelanjohps@gmail.com>
2023-04-25 16:07:47 -0300

    feat: :sparkles: Cria a classe CarODM
    Com método para retornar os carros cadastrados no banco.

---
+++ src/Models/CarODM.ts
@@ -0,0 +1,26 @@
+import { Model, Schema, model, models } from 'mongoose';
+import ICar from '../Interfaces/ICar';
+
+const carSchema = new Schema<ICar>({
+  model: { type: String, required: true },
+  year: { type: Number, required: true },
+  color: { type: String, required: true },
+  buyValue: { type: Number, required: true },
+  doorsQty: { type: Number, required: true },
+  seatsQty: { type: Number, required: true },
+  status: { type: Boolean, required: false, default: false },
+});
+
+class CarODM {
+  private model: Model<ICar>;
+
+  constructor() {
+    this.model = models.Car || model('Car', carSchema);
+  }
+
+  public async findAll(): Promise<ICar[]> {
+    return this.model.find();
+  }
+}
+
+export default CarODM;
